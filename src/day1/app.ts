29aea54e432fa912add2705aa5c6bd240c84ae65
Fazzacoot <farongottlieb@gmail.com>
2020-01-24 11:33:44 +0200

    Day 1

---
+++ src/day1/app.ts
@@ -0,0 +1,25 @@
+//Day 1
+
+//Comments will make code more redable
+//30 Days of Javascript but written in Typescript
+/**
+ * Lets learn together
+ * and achieve greatness
+ */
+let maritalStatus:string, country:string;
+maritalStatus = "Single";
+country = "ZA";
+
+let fullName = "Faron Gottlieb"; //TS inferet string type
+let age = 24; //TS inferet number type
+let playsVideoGames = true; //TS inferet boolean type
+let isNull = null; //TS inferet any type
+let isUndefinded = undefined; //TS inferet undefined type
+
+console.log(`${fullName} is of type ${typeof fullName}`)
+console.log(`${age} is of type ${typeof age}`)
+console.log(`${playsVideoGames} is of type ${typeof playsVideoGames}`)
+console.log(`${isNull} is of type ${typeof isNull}`)
+console.log(`${isUndefinded} is of type ${typeof isUndefinded}`)
+
+//Day 1
